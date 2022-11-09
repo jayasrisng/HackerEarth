b272a945cf8d9b9a727830ad40f1079c2513386d
Guthula Jayasri Sai Nikitha <54980994+jayasrisng@users.noreply.github.com>
2022-11-09 23:29:24 +0530

    Create Xsquare_and_Coin_Collection.py

---
+++ Xsquare_and_Coin_Collection.py
@@ -0,0 +1,30 @@
+'''
+Problem
+Xsquare loves to play with the coins very much. Today , he has N stacks of coins . Each stack of coins has some non zero height Hi equal to the number of coins on that stack ( considering all the coins are identical and each coin has a height of 1 unit ) .
+
+In one move, Xsquare can select any number of consecutive stacks of coins such that the height of each selected stack of coins Hi ≤ K . Once such a sequence of stacks is chosen , Xsquare can collect any number of coins from the chosen sequence of stacks .
+
+Xsquare wonders what is the maximum number of coins that he can collect this way ?
+
+INPUT
+First line of input contains a single integer T denoting the number of test cases . First line of each test case contains two space separated integers N and K where N being the number of stacks of coins. Second line of each test case contains N space separated integers denoting the number of coins Hi on each stack .
+
+OUTPUT
+For each test case , Print the maximum number of coins Xsquare can collect following the above gaming strategy.
+'''
+
+def coins(n,h,k):
+    a=b=0
+    for i in h:
+        if(i<=k):
+            a+=i
+            b=max(a,b)
+        else:
+            a=0
+    print(b)
+ 
+t=int(input())
+for i in range(t):
+    n,k=map(int,input().split())
+    h=list(map(int,input().split()))
+    coins(n,h,k)
